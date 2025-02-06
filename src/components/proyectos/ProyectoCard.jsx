b7765d9feb3f1ed549fd037a05443f34ad62e401
Carlos Santana <carlossantana331@gmail.com>
2025-02-05 20:13:43 -0600

    se trabajan las cards de los proyectos

--- src/components/proyectos/ProyectoCard.jsx
+++ src/components/proyectos/ProyectoCard.jsx
@@ -1,25 +1,62 @@
 import React from "react";
 import proyectos from "../../data/poyectos.json";
+import "./ProyectoCard.css";
+import tecnologiasLogos from  "../../data/tecnologiasLogos.js";
+import { FaGithubSquare } from "react-icons/fa";
+import { TbWorldWww } from "react-icons/tb";
+
 
 function ProyectoCard() {
     return (
-        <div>
-            {proyectos.map((proyecto, index) => (
-                <div key={index} className="proyecto-card">
-                    <img src={proyecto.imagen} alt={proyecto.titulo} />
-                    <h3>{proyecto.titulo}</h3>
-                    <p>{proyecto.descripcion}</p>
-                    <p>
-                        <strong>Tecnologías:</strong> {proyecto.tecnologias.join(", ")}
-                    </p>
-                    <a href={proyecto.repo} target="_blank" rel="noopener noreferrer">
-                        Ver código
-                    </a>
-                    <a href={proyecto.deploy} target="_blank" rel="noopener noreferrer">
-                        Ver demo
-                    </a>
-                </div>
-            ))}
+        <div className="proyecto-container">
+            <h2 className="proyecto-title">Proyectos</h2>
+
+            <div className="proyecto-card-container">
+                {proyectos.map((proyecto) => (
+                    <div key={proyecto.id} className="proyecto-card">
+                        <h3 className="proyecto-name">{proyecto.titulo}</h3>
+                        <div className="proyecto-card-hoover">
+                            <div className="proyecto-image">
+                                <img src={proyecto.imagen} alt={proyecto.titulo} />
+                            </div>
+
+                            <div className="proyecto-description">
+                                <div className="proyecto-icon"></div>
+                                <p className="proyecto-text">{proyecto.descripcion}</p>
+
+                            </div>
+                        </div>
+
+                        <div className="proyecto-tecnologias-container">
+                            {proyecto.tecnologias.map((tec ,index) => {
+                                const Icono = tecnologiasLogos[tec];
+                                return(
+                                    <div key={index} className="tooltip">
+                                        <span className={`proyecto-logo ${tec} `}>
+                                            {Icono ? <Icono /> : tec}
+                                        </span>
+                                        <span className={`tooltiptext tooltip${tec}`}>{tec}</span>
+
+                                    </div>
+                                );
+                            })}
+                        </div>
+
+                        <div className="proyecto-links">
+                            <a className="tooltip" href={proyecto.repo} target="_blank" rel="noopener noreferrer">
+                                <FaGithubSquare />
+                                <p className="tooltiptext tooltipGoogleDriveAPI">visitar repositorio</p>
+                            </a>
+                            
+                            <a className="tooltip"href={proyecto.deploy} target="_blank" rel="noopener noreferrer">
+                                <TbWorldWww />
+                                <p className="tooltiptext tooltipGoogleDriveAPI">ver deploy</p>
+                            </a>
+                        </div>
+                    </div>
+                ))}
+
+            </div>
         </div>
     );
 }
---
+++ src/data/tecnologiasLogos.js
@@ -0,0 +1,17 @@
+import { FaHtml5, FaReact, FaCss3Alt, FaBootstrap, FaSass, FaGoogleDrive, FaJs } from "react-icons/fa";
+import { IoLogoFirebase } from "react-icons/io5";
+import { TbBrandVite } from "react-icons/tb";
+
+const tecnologiasLogos = {
+  HTML: FaHtml5,
+  React: FaReact,
+  Firebase: IoLogoFirebase,
+  CSS: FaCss3Alt,
+  Vite: TbBrandVite,
+  Bootstrap: FaBootstrap,
+  Sass: FaSass,
+  GoogleDriveAPI: FaGoogleDrive,
+  JS: FaJs
+};
+
+export default tecnologiasLogos;
